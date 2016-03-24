a5a2558f5cd9a5fafe3817ecd4dbde9fba19ec49
Yacent <540608491@qq.com>
2016-03-24 13:22:38 +0800

    添加了drapdrop自定义事件的实现

--- drap-drop/drag.js
+++ drap-drop/drag.js
@@ -1,5 +1,45 @@
 window.onload = function() {
+	// 定义自定义事件
+	function EventTarget() {
+		this.handlers = {};
+	}
+
+	EventTarget.prototype = {
+		constructor: EventTarget,
+		addHandler: function(type, handler) {
+			if (typeof this.handlers[type] == "undefined") {
+				this.handlers[type] = [];
+			}
+			this.handlers[type].push(handler);
+		},
+
+		fire: function(event) {
+			if (event.target) {
+				event.target = this;
+			}
+			if (this.handlers[event.type] instanceof Array) {
+				var handlers = this.handlers[event.type];
+				for (var i = 0, len = handlers.length; i < len; i++) {
+					handlers[i](event);
+				}
+			}
+		},
+
+		removeHandler: function(type, handler) {
+			if (this.handlers[type] instanceof Array) {
+				var handlers = this.handlers[type];
+				for (var i = 0, len = handlers.length; i < len; i++) {
+					if (handlers[i] === handler) {
+						break;
+					}
+				}
+				handlers.splice(i, 1);
+			}
+		}
+	};
+
 	var DragDrop = function() {
+		var drapdrop = new EventTarget();
 		var dragging = null;
 		var diffX = 0;
 		var diffY = 0;
@@ -7,12 +47,17 @@ window.onload = function() {
 		function handleEvent(event) {
 			var that = event.target;
 
+			// 确定事件的类型
 			switch(event.type) {
 				case "mousedown":
 					if (that.className.indexOf("draggable") > -1) {
 						dragging = that;
 						diffX = event.clientX - that.offsetLeft;
 						diffY = event.clientY - that.offsetTop;
+						drapdrop.fire({type: "dragstart",
+									   target: dragging,
+									   x: event.clientX,
+									   y: event.clientY});
 					}
 					break;
 
@@ -20,28 +65,38 @@ window.onload = function() {
 					if (dragging !== null) {
 						dragging.style.left = (event.clientX - diffX) + "px";
 						dragging.style.top = (event.clientY - diffY) + "px";
+
+						drapdrop.fire({type: "drag",
+									   target: dragging,
+									   x: event.clientX,
+									   y: event.clientY});
 					}
 					break;
 
 				case "mouseup":
+					drapdrop.fire({type: "dragend",
+								   target: dragging,
+								   x: event.clientX,
+								   y: event.clientY});
 					dragging = null;
 					break;
 			}
 		};
 
-		return {
-			enable : function() {
-				document.addEventListener("mousedown", handleEvent);
-				document.addEventListener("mousemove", handleEvent);
-				document.addEventListener("mouseup", handleEvent);
-			},
+		drapdrop.enable = function() {
+			document.addEventListener("mousedown", handleEvent);
+			document.addEventListener("mousemove", handleEvent);
+			document.addEventListener("mouseup", handleEvent);
+		},
 
-			disable : function() {
-				document.removeEventListener("mousedown", handleEvent);
-				document.removeEventListener("mousemove", handleEvent);
-				document.removeEventListener("mouseup", handleEvent);
-			}
+		drapdrop.disable = function() {
+			document.removeEventListener("mousedown", handleEvent);
+			document.removeEventListener("mousemove", handleEvent);
+			document.removeEventListener("mouseup", handleEvent);
 		}
+
+		return drapdrop;
+
 	}();
 
 	DragDrop.enable();
